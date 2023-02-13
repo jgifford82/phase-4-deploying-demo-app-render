fc1229719ae1370e888b1e06b22ca62f5371724a
Jennifer Gifford <jgifford82@gmail.com>
2023-02-13 16:52:42 -0700

    text update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -9,7 +9,7 @@ function Login({ onLogin }) {
 
   return (
     <Wrapper>
-      <Logo>Reciplease</Logo>
+      <Logo>Reciplease?!</Logo>
       {showLogin ? (
         <>
           <LoginForm onLogin={onLogin} />
